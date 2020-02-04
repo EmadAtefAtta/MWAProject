6d3a4e44e8f90d3a5d96ee84bcfcdc3f4d7332e3
Hedra <Hedra_gerges@live.com>
2020-02-04 14:46:16 -0600

    add gard to protected URL

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AdminAddProductComponent } from './admin-add-product/admin-add-product.component';
 import { AdminListComponent } from './admin-list/admin-list.component';
+import { ProtectedGuard } from './_guards/protected.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'addAdmin', component: AdminAddProductComponent },
-  {path:'listAdmin',component: AdminListComponent}
+  { path: 'addAdmin', component: AdminAddProductComponent, canActivate: [ProtectedGuard] },
+  {path:'listAdmin',component: AdminListComponent , canActivate: [ProtectedGuard]}
 ];
 
 @NgModule({
